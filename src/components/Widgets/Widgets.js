6567b79fb45cf8e7676ccd06bca4029908c65194
Ihuoma Ndukwe <chineme4g@gmail.com>
2022-04-01 20:41:21 +0100

    started widgets

--- src/components/Widgets/Widgets.js
+++ src/components/Widgets/Widgets.js
@@ -9,7 +9,7 @@ function Widgets() {
       </div>
       <div className='widgetsContainer'>
         <h2>What's happening</h2>
-        <TwitterTweetEmbed tweetId={1508516760059846659}/>
+        
       </div>
   </div>;
 }
